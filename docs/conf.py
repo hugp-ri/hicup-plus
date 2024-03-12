4a1cc6a221d6dcbe9f9a4ff24abcd40ec999a8c2
Simon Thomas Kelly <simonthomas.kelly@hugp.com>
2024-03-12 13:26:38 +0900

    update ReadTheDocs (test)

--- docs/conf.py
+++ docs/conf.py
@@ -1,153 +1,51 @@
 # -*- coding: utf-8 -*-
 
-from recommonmark.parser import CommonMarkParser
 
 extensions = []
-templates_path = ['/home/docs/checkouts/readthedocs.org/readthedocs/templates/sphinx', 'templates', '_templates', '.templates']
-source_suffix = ['.rst', '.md']     
-source_parsers = {      
-            '.md': CommonMarkParser,        
-        }
+templates_path = ['_templates']
+source_suffix = ['.rst']		
 master_doc = 'index'
-project = u'jython'
-copyright = u'2016'
+
+project = u'HiCUP+'
+copyright = u'2010, 2019'
 version = 'latest'
 release = 'latest'
-exclude_patterns = ['_build']
-pygments_style = 'sphinx'
-htmlhelp_basename = 'jython'
-html_theme = 'sphinx_rtd_theme'
-file_insertion_enabled = False
-latex_documents = [
-  ('index', 'jython.tex', u'jython Documentation',
-   u'', 'manual'),
+
+# Add any Sphinx extension module names here, as strings.
+extensions = [
+    'sphinx.ext.intersphinx',
 ]
 
+exclude_patterns = ['_build','sandbox']
+
+pygments_style = 'sphinx'
 
 
+# -- Options for HTML output -------------------------------------------------
 
-###########################################################################
-#          auto-created readthedocs.org specific configuration            #
-###########################################################################
-
-
-#
-# The following code was added during an automated build on readthedocs.org
-# It is auto created and injected for every build. The result is based on the
-# conf.py.tmpl file found in the readthedocs.org codebase:
-# https://github.com/rtfd/readthedocs.org/blob/master/readthedocs/doc_builder/templates/doc_builder/conf.py.tmpl
-#
+html_theme = 'sphinx_rtd_theme'
+file_insertion_enabled = False
 
 
-import sys
-import os.path
-from six import string_types
+# -- Options for HTMLHelp output ---------------------------------------------
 
-from sphinx import version_info
-
-# Get suffix for proper linking to GitHub
-# This is deprecated in Sphinx 1.3+,
-# as each page can have its own suffix
-if globals().get('source_suffix', False):
-    if isinstance(source_suffix, string_types):
-        SUFFIX = source_suffix
-    else:
-        SUFFIX = source_suffix[0]
-else:
-    SUFFIX = '.rst'
-
-# Add RTD Static Path. Add to the end because it overwrites previous files.
-if not 'html_static_path' in globals():
-    html_static_path = []
-if os.path.exists('_static'):
-    html_static_path.append('_static')
-html_static_path.append('/home/docs/checkouts/readthedocs.org/readthedocs/templates/sphinx/_static')
-
-# Add RTD Theme only if they aren't overriding it already
-using_rtd_theme = False
-if 'html_theme' in globals():
-    if html_theme in ['default']:
-        # Allow people to bail with a hack of having an html_style
-        if not 'html_style' in globals():
-            import sphinx_rtd_theme
-            html_theme = 'sphinx_rtd_theme'
-            html_style = None
-            html_theme_options = {}
-            if 'html_theme_path' in globals():
-                html_theme_path.append(sphinx_rtd_theme.get_html_theme_path())
-            else:
-                html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-            using_rtd_theme = True
-else:
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_style = None
-    html_theme_options = {}
-    if 'html_theme_path' in globals():
-        html_theme_path.append(sphinx_rtd_theme.get_html_theme_path())
-    else:
-        html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-    using_rtd_theme = True
-
-if globals().get('websupport2_base_url', False):
-    websupport2_base_url = 'https://readthedocs.org/websupport'
-    if 'http' not in settings.MEDIA_URL:
-        websupport2_static_url = 'https://media.readthedocs.org/static/'
-    else:
-        websupport2_static_url = 'https://media.readthedocs.org//static'
+htmlhelp_basename = 'hicup-plus'
 
 
-#Add project information to the template context.
-context = {
-    'using_theme': using_rtd_theme,
-    'html_theme': html_theme,
-    'current_version': "latest",
-    'MEDIA_URL': "https://media.readthedocs.org/",
-    'PRODUCTION_DOMAIN': "readthedocs.org",
-    'versions': [
-    ("latest", "/en/latest/"),
-    ],
-    'downloads': [ 
-    ("pdf", "//readthedocs.org/projects/jython/downloads/pdf/latest/"),
-    ("htmlzip", "//readthedocs.org/projects/jython/downloads/htmlzip/latest/"),
-    ("epub", "//readthedocs.org/projects/jython/downloads/epub/latest/"),
-    ],
-    'subprojects': [ 
-    ],
-    'slug': 'jython',
-    'name': u'jython',
-    'rtd_language': u'en',
-    'canonical_url': 'http://jython.readthedocs.io/en/latest/',
-    'analytics_code': 'None',
-    'single_version': False,
-    'conf_py_path': '/./',
-    'api_host': 'https://readthedocs.org',
-    'github_user': 'jython',
-    'github_repo': 'book',
-    'github_version': 'master',
-    'display_github': True,
-    'bitbucket_user': 'None',
-    'bitbucket_repo': 'None',
-    'bitbucket_version': 'master',
-    'display_bitbucket': False,
-    'READTHEDOCS': True,
-    'using_theme': (html_theme == "default"),
-    'new_theme': (html_theme == "sphinx_rtd_theme"),
-    'source_suffix': SUFFIX,
-    'user_analytics_code': '',
-    'global_analytics_code': 'UA-17997319-1',
+# -- Options for LaTeX output ------------------------------------------------
+# LaTeX is intermediate to the PDF output
 
-    'commit': 'cf5cf6de',
+latex_documents = [
+    # (startdocname, targetname, title, author, documentclass, toctree_only)
+    (   'index', 'index.tex',
+        u'HiCUP+',
+        u'S. Thomas Kelly, Satoshi Yuhara',
+        'manual',
+        False),
+]
 
+# -- Options for intersphinx extension ---------------------------------------
+
+intersphinx_mapping = {
+    'python' : ('https://docs.python.org/2', None),
 }
-if 'html_context' in globals():
-    html_context.update(context)
-else:
-    html_context = context
-
-# Add custom RTD extension
-if 'extensions' in globals():
-    extensions.append("readthedocs_ext.readthedocs")
-else:
-    extensions = ["readthedocs_ext.readthedocs"]
